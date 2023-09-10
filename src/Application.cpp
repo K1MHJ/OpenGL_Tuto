370baae55dd571f5869256e9b5b86e47b3bc823c
K1MHJ <hojokimu@gmail.com>
2023-09-10 13:17:07 +0900

    add Uniforms in shader

--- src/Application.cpp
+++ src/Application.cpp
@@ -128,10 +128,13 @@ int main(void)
 
   /* Make the window's context current */
   glfwMakeContextCurrent(window);
+  
+  glfwSwapInterval(1);
 
   if(glewInit() != GLEW_OK){
     std::cout << "Error!" << std::endl;
   }
+
   std::cout << glGetString(GL_VERSION) << std::endl;
   
   float positions[] = {
@@ -166,6 +169,13 @@ int main(void)
   ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
   const GLuint shader(CreateShader(source.VertexSource, source.FragmentSource));
   glUseProgram(shader);
+  
+  GLCall(int location = glGetUniformLocation(shader, "u_Color"));
+  ASSERT(location != -1);
+  GLCall(glUniform4f(location, 0.8f, 0.3f, 0.8f, 1.0f));
+  
+  float r = 0.0f;
+  float increment = 0.05;
 
   /* Loop until the user closes the window */
   while (!glfwWindowShouldClose(window))
@@ -174,8 +184,16 @@ int main(void)
     glClear(GL_COLOR_BUFFER_BIT);
 
     glBindVertexArray(vao);
-  
+
+    GLCall(glUniform4f(location,r, 0.3f, 0.8f, 1.0f));
     GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+    
+    if(r>1.0f)
+      increment = -0.05f;
+    else if ( r < 0.0f)
+      increment = 0.05f;
+
+    r += increment;
 
     /* Swap front and back buffers */
     glfwSwapBuffers(window);
