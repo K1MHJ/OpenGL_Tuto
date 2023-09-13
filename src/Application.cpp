9afa0b987652ea03350bef7212d33866ae4074ca
K1MHJ <hojokimu@gmail.com>
2023-09-14 00:35:43 +0900

    writte Textures

--- src/Application.cpp
+++ src/Application.cpp
@@ -15,6 +15,10 @@
 #include "VertexBuffer.hpp"
 #include "VertexArray.hpp"
 #include "Shader.hpp"
+#include "Texture.hpp"
+
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
 
 int main(void)
 {
@@ -50,10 +54,10 @@ int main(void)
   std::cout << glGetString(GL_VERSION) << std::endl;
   {
     float positions[] = {
-      -0.5f, -0.5f,
-       0.5f, -0.5f,
-       0.5f,  0.5f,
-      -0.5f,  0.5f
+      -0.5f, -0.5f, 0.0f, 0.0f, //0
+       0.5f, -0.5f, 1.0f, 0.0f, //1
+       0.5f,  0.5f, 1.0f, 1.0f, //2
+      -0.5f,  0.5f, 0.0f, 1.0f  //3
     };
     
     unsigned int indices[] = {
@@ -61,9 +65,14 @@ int main(void)
       2,3,0
     };
 
+    GLCall(glEnable(GL_BLEND));
+    GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+
     VertexArray va;
-    VertexBuffer vb(positions, 4 * 2 * sizeof(float));
+    VertexBuffer vb(positions, 4 * 4 * sizeof(float));
+
     VertexBufferLayout layout;
+    layout.Push<float>(2);
     layout.Push<float>(2);
     va.AddBuffer(vb, layout);
     
@@ -72,6 +81,10 @@ int main(void)
     Shader shader("res/shaders/Basic.shader");
     shader.Bind();
     shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+    
+    Texture texture("res/textures/logo.png");
+    texture.Bind();
+    shader.SetUniform1i("u_Texture", 0);
 
     va.Unbind();
     vb.Unbind();
--- src/Shader.cpp
+++ src/Shader.cpp
@@ -26,27 +26,17 @@ void Shader::Unbind() const
   GLCall(glUseProgram(0));
 }
 
+void Shader::SetUniform1i(const std::string& name, int value)
+{
+  GLCall(glUniform1i(GetUniformLocation(name), value));
+}
 void Shader::SetUniform1f(const std::string& name, float value)
 {
   GLCall(glUniform1f(GetUniformLocation(name), value));
 }
-
 void Shader::SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3)
 {
   GLCall(glUniform4f(GetUniformLocation(name), v0, v1, v2, v3));
-}
-
-unsigned int Shader::GetUniformLocation(const std::string& name)
-{
-  if(m_UniformLocationCache.find(name) != m_UniformLocationCache.end())
-    return m_UniformLocationCache[name];
-
-  GLCall(unsigned int location = glGetUniformLocation(m_RendererID, name.c_str()))
-  if(location == -1)
-    std::cout << "Warning: uniform '" << name << "' doesn't exit!" << std::endl;
-
-  m_UniformLocationCache[name] = location;
-  return location;
 }
 
 ShaderProgramSource Shader::ParseShader(const std::string& filepath)
@@ -117,4 +107,17 @@ unsigned int Shader::CreateShader(const std::string& vertexShader, const std::st
   glDeleteShader(fs);
 
   return program;
+}
+
+int Shader::GetUniformLocation(const std::string& name)
+{
+  if(m_UniformLocationCache.find(name) != m_UniformLocationCache.end())
+    return m_UniformLocationCache[name];
+
+  GLCall(int location = glGetUniformLocation(m_RendererID, name.c_str()))
+  if(location == -1)
+    std::cout << "Warning: uniform '" << name << "' doesn't exit!" << std::endl;
+
+  m_UniformLocationCache[name] = location;
+  return location;
 }
